52a27a10db790663ce1151814db1e6ee19bb7708
abbystone <manasbhatt007@gmail.com>
2019-11-03 14:14:40 +0530

    UI with new dialog up

---
+++ src/com/abysstone/contactsApplication/ContactController.java
@@ -0,0 +1,31 @@
+package com.abysstone.contactsApplication;
+
+import com.abysstone.contactsApplication.datamodel.Contact;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+public class ContactController {
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField notesField;
+
+    public Contact getNewContact() {
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        String notes = notesField.getText();
+
+        Contact newContact = new Contact(firstName, lastName, phoneNumber, notes);
+        return newContact;
+    }
+}
